c17193c49465fe0c861b3d6f1ba61a06ca4a26b7
listen-1994 <listenmail@yeah.net>
2018-12-11 10:18:09 +0800

    44

--- src/DynamicProgramming/WildcardMatching.java
+++ src/DynamicProgramming/WildcardMatching.java
@@ -1,5 +1,9 @@
 package DynamicProgramming;
 
+/**
+ * LeetCode 第44题
+ * 通配符匹配
+ */
 public class WildcardMatching {
 
     boolean[][] sav;
@@ -8,42 +12,44 @@ public class WildcardMatching {
         sav[0][0]=true;
 
         for(int i=1;i<=p.length();i++){
+            if(p.charAt(i-1)=='*'){
+                sav[0][i]=sav[0][i-1];
+            }else{
+                sav[0][i]=false;
+            }
+        }
+        for(int i=1;i<=p.length();i++){
             for(int j=1;j<=s.length();j++){
                 if(p.charAt(i-1)=='?'){
                     sav[j][i]=sav[j-1][i-1];
                 }else if(p.charAt(i-1)=='*'){
-                    for(int k=)
+                    for(int k=j;k>=0;k--){
+                        sav[j][i]=sav[j][i]||sav[k][i-1];
+                    }
+                }else{
+                    if(p.charAt(i-1)==s.charAt(j-1)){
+                        sav[j][i]=sav[j-1][i-1];
+                    }else{
+                        sav[j][i]=false;
+                    }
                 }
             }
         }
+        //print(sav);
+        return sav[s.length()][p.length()];
     }
 
-
-    public void core(String s,String p,int index_s,int index_p){
-        if(index_s==s.length()&&index_p==p.length()){
-            return;
-        }
-        if(index_s==s.length()&&index_p!=p.length()){
-            if(p.charAt(index_p)!='*'){
-                sav[index_s][index_p+1]=false;
-            }else{
-                sav[index_s][index_p+1]=true;
-                core(s,p,index_s,index_p+1);
+    public void print(boolean[][] sav){
+        for(int i=0;i<sav.length;i++){
+            for(int j=0;j<sav[i].length;j++){
+                System.out.print(sav[i][j]+" ");
             }
-        }
-        if(index_p==p.length()&&index_s!=s.length()){
-            sav[index_s+1][index_p]=false;
-            core(s,p,index_s+1,index_p);
-        }
-
-        if(p.charAt(index_p)=='?'){
-            sav[index_s+1][index_p+1]=sav[index_s][index_p];
+            System.out.println("\n");
         }
     }
-
 
     public static void main(String[] args){
         WildcardMatching wildcardMatching = new WildcardMatching();
-        System.out.println(wildcardMatching.isMatch("aaaa","?aab"));
+        System.out.println(wildcardMatching.isMatch("adceb","*a*b"));
     }
 }
