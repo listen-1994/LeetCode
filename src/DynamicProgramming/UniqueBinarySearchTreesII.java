d2e5e1173a1876dad0783e21d3de5497b14c9fb5
listen-1994 <listenmail@yeah.net>
2018-12-16 17:41:50 +0800

    不同的二叉搜索树

---
+++ src/DynamicProgramming/UniqueBinarySearchTreesII.java
@@ -0,0 +1,27 @@
+package DynamicProgramming;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * LeetCode 第95题
+ * 不同的二叉搜索树
+ */
+public class UniqueBinarySearchTrees {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    class Solution {
+        List<TreeNode> result = new LinkedList<>();
+        public List<TreeNode> generateTrees(int n) {
+            List<TreeNode> list = new LinkedList<>();
+            for(int i=0;i<)
+        }
+    }
+}
