163a38d4a830fc136ac75fbc96164a895727c69d
listen <listenmail@yeah.net>
2019-10-22 09:21:29 +0800

    '修改代码'

---
+++ src/Problem77.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * LeetCode 第77题 组合
+ * 递归问题
+ */
+public class Problem76 {
+
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new LinkedList<>();
+        core(result, 1, n, k);
+        return result;
+    }
+
+    public void core(List<List<Integer>> result, int begin, int end, int k) {
+        if (k > end - begin + 1 || k == 0) {
+            return;
+        }
+        for (int i = begin; i <= end - k + 1; i++) {
+            List<List<Integer>> subResult = new LinkedList<>();
+            core(subResult, i + 1, end, k - 1);
+            if(subResult.isEmpty()){
+                List<Integer> thisResult = new LinkedList<>();
+                thisResult.add(i);
+                result.add(thisResult);
+            }
+            for (List<Integer> list : subResult) {
+                List<Integer> thisResult = new LinkedList<>();
+                thisResult.add(i);
+                thisResult.addAll(list);
+                result.add(thisResult);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Problem76 problem76 = new Problem76();
+        System.out.println(problem76.combine(4,2));
+    }
+}
