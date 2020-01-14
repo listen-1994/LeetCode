f56267b94c39cb7814d9b44ee78b2146919d7956
sheng.li <sheng.li@weimob.com>
2020-01-14 18:54:55 +0800

    【109题】

--- src/Problem224.java
+++ src/Problem224.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class Problem224 {
@@ -11,15 +13,19 @@ public class Problem224 {
             return 0;
         }
         //存放后缀
-        Stack<String> s2 = new Stack<>();
+        List<String> s2 = new ArrayList<>();
         //存放操作数
         Stack<String> s1 = new Stack<>();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
             char currentChar = s.charAt(i);
+            if (currentChar==' '){
+                continue;
+            }
             if (checkOperateChar(currentChar)) {
-                sb.append(currentChar);
-                s2.push(sb.toString());
+                if(sb.length()!=0){
+                    s2.add(sb.toString());
+                }
                 sb = new StringBuilder();
                 String currentOperateChar = s1.isEmpty()?null:s1.peek();
                 if (currentChar == leftBrackets) {
@@ -34,24 +40,57 @@ public class Problem224 {
                     if (stringEqualsChar(currentOperateChar, plus) || stringEqualsChar(currentOperateChar, minus)) {
                         s1.pop();
                         s1.push(String.valueOf(currentChar));
-                        s2.push(currentOperateChar);
+                        s2.add(currentOperateChar);
                         continue;
                     }
                 }
                 if (currentChar == rightBrackets) {
+                    while(!s1.peek().equals(String.valueOf(leftBrackets))){
+                        s1.pop();
+                    }
                     s1.pop();
-                    s1.pop();
-                    s2.push(currentOperateChar);
+                    if (!currentOperateChar.equals(String.valueOf(leftBrackets))){
+                        s2.add(currentOperateChar);
+                    }
                 }
             } else {
                 sb.append(currentChar);
             }
         }
-        return 0;
+        if (sb.length()!=0){
+            s2.add(sb.toString());
+        }
+        while(!s1.isEmpty()){
+            s2.add(s1.pop());
+        }
+        Stack<Integer> integerStack = new Stack<>();
+        Integer operate1 = 0;
+        Integer operate2 = 0;
+        for (String s3 : s2) {
+            if (checkOperateChar(s3)){
+                operate1 = integerStack.pop();
+                operate2 = integerStack.pop();
+                int result = 0;
+                if (s3.equals(String.valueOf(plus))){
+                    result = operate1+operate2;
+                }else if (s3.equals(String.valueOf(minus))) {
+                    result = operate2 - operate1;
+                }
+                integerStack.push(result);
+            }else{
+                integerStack.push(Integer.valueOf(s3));
+            }
+        }
+
+        return integerStack.pop();
     }
 
     private boolean checkOperateChar(char checkChar) {
         return checkChar == plus || checkChar == minus || checkChar == leftBrackets || checkChar == rightBrackets;
+    }
+
+    private boolean checkOperateChar(String checkString){
+        return checkString.equals(String.valueOf(plus)) || checkString.equals(String.valueOf(minus)) || checkString.equals(String.valueOf(leftBrackets)) || checkString.equals(String.valueOf(rightBrackets));
     }
 
     private boolean stringEqualsChar(String checkString, char checkChar) {
@@ -63,6 +102,7 @@ public class Problem224 {
 
     public static void main(String[] args) {
         Problem224 problem224 = new Problem224();
-        problem224.calculate("(1+(4+5+2)-3)+(6+8)");
+        Integer result = problem224.calculate("(１)");
+        System.out.println(result);
     }
 }
