41e745e26a200faca29c8dccdccbddb4bd763e69
sheng.li <listenmail@yeah.net>
2020-03-13 20:17:06 +0800

    233

---
+++ src/Problem114.java
@@ -0,0 +1,51 @@
+import LinkedList.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Problem114 {
+/*    public void flatten(TreeNode root) {
+        List<TreeNode> nodeList = new ArrayList<>();
+        dfs(root, nodeList);
+        for (int i = 0; i < nodeList.size() - 1; i++) {
+            nodeList.get(i).left = null;
+            nodeList.get(i).right = nodeList.get(i + 1);
+        }
+    }
+
+    private void dfs(TreeNode root, List<TreeNode> nodeList) {
+        if (null != root) {
+            nodeList.add(root);
+            dfs(root.left, nodeList);
+            dfs(root.right, nodeList);
+        }
+    }*/
+
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root);
+    }
+
+    private TreeNode dfs(TreeNode root) {
+        if (null == root.left && null == root.right) {
+            return root;
+        }
+        TreeNode spliceNode = null;
+        if (null != root.left) {
+            spliceNode = dfs(root.left);
+        }
+        TreeNode endNode = spliceNode;
+        if (null != root.right) {
+            endNode = dfs(root.right);
+        }
+        if (null != spliceNode) {
+            spliceNode.right = root.right;
+            root.right = root.left;
+            root.left = null;
+        }
+        return endNode;
+    }
+}
