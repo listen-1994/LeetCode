b3f20108071651d663989160efea5e7e9f2a685f
sheng.li <sheng.li@weimob.com>
2020-04-07 23:53:57 +0800

    282

--- src/Problem295.java
+++ src/Problem295.java
@@ -2,17 +2,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem295 {
-    class MedianFinder {
+    //第一次提交
+/*    static class MedianFinder {
 
         Integer medianIndex;
         Boolean flag;
-        List<Integer> numList;
+        List<Double> numList;
 
-        /**
-         * initialize your data structure here.
-         */
+        */
+
+    /**
+     * initialize your data structure here.
+     *//*
         public MedianFinder() {
-            medianIndex = 0;
+            medianIndex = -1;
             flag = true;
             numList = new ArrayList<>();
         }
@@ -29,28 +32,108 @@ public class Problem295 {
             if (checkOddSize()) {
                 insertNum(num);
             } else {
-                
+                insertNum(num);
+                medianIndex++;
             }
             flag = !flag;
         }
 
         public double findMedian() {
-            return 0L;
+            if (numList.isEmpty()) {
+                return 0L;
+            }
+            System.out.println("medianIndex:" + medianIndex);
+            System.out.println(numList);
+            if (checkOddSize()) {
+                return numList.get(medianIndex);
+            }
+            return (numList.get(medianIndex) + numList.get(medianIndex + 1)) / 2;
         }
 
-        public void insertNum(int num) {
+        public void insertNum(double num) {
             int beginSize = numList.size();
+            boolean added = false;
             for (int i = 0; i < beginSize; i++) {
                 if (numList.get(i) >= num) {
                     numList.add(i, num);
+                    added = true;
+                    break;
                 }
             }
-            if (beginSize == numList.size()) {
+            if (!added) {
                 numList.add(num);
             }
         }
 
+    }*/
+
+    static class MedianFinder {
+        TreeNode root;
+        boolean oddFlag;
+
+        class TreeNode {
+            TreeNode left;
+            TreeNode right;
+            int val1;
+            Integer val2;
+
+            TreeNode(int val1) {
+                this.val1 = val1;
+            }
+        }
+
+        /**
+         * initialize your data structure here.
+         */
+        public MedianFinder() {
+
+        }
+
+        public void addNum(int num) {
+            if (root == null) {
+                root = new TreeNode(num);
+            } else {
+                insertNum(num, root);
+            }
+            if (num <= root.val1) {
+            } else {
+            }
+
+            oddFlag = !oddFlag;
+        }
+
+        public TreeNode insertNum(int num, TreeNode root) {
+            if (num <= root.val) {
+                if (null == root.left) {
+                    root.left = new TreeNode(num);
+                } else {
+                    insertNum(num, root.left);
+                }
+            } else {
+                if (null == root.right) {
+                    root.right = new TreeNode(num);
+                } else {
+                    insertNum(num, root.right);
+                }
+            }
+        }
+
+        public double findMedian() {
+            if (oddFlag) {
+                return (double) root.val;
+            }
+            return (double) (root.val + root.right.val) / 2;
+        }
     }
 
+    public static void main(String[] args) {
+        MedianFinder medianFinder = new MedianFinder();
 
+        Double result;
+        for (int i = -1; i > -6; i--) {
+            medianFinder.addNum(i);
+            result = medianFinder.findMedian();
+            System.out.println(result);
+        }
+    }
 }
