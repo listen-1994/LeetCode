255988047918f7555ad5d99359e96711ebf1fadb
sheng.li <sheng.li@weimob.com>
2020-04-11 20:06:47 +0800

    282

--- src/Problem295.java
+++ src/Problem295.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem295 {
@@ -67,18 +68,24 @@ public class Problem295 {
 
     }*/
 
+    /**
+     * 使用树型结构动态调整，查询时间变为log（n）
+     */
     static class MedianFinder {
         TreeNode root;
         boolean oddFlag;
+        int leftNum;
+        int rightNum;
 
         class TreeNode {
             TreeNode left;
             TreeNode right;
-            int val1;
-            Integer val2;
+            TreeNode father;
+            int val;
 
-            TreeNode(int val1) {
-                this.val1 = val1;
+            TreeNode(int val, TreeNode father) {
+                this.val = val;
+                this.father = father;
             }
         }
 
@@ -91,38 +98,118 @@ public class Problem295 {
 
         public void addNum(int num) {
             if (root == null) {
-                root = new TreeNode(num);
+                root = new TreeNode(num, null);
             } else {
+                if (num < root.val) {
+                    leftNum++;
+                } else {
+                    rightNum++;
+                }
                 insertNum(num, root);
             }
-            if (num <= root.val1) {
-            } else {
-            }
 
+            reBalance();
             oddFlag = !oddFlag;
         }
 
-        public TreeNode insertNum(int num, TreeNode root) {
-            if (num <= root.val1) {
-                if (null == root.left) {
-                    root.left = new TreeNode(num);
+        public TreeNode findLeftMax(TreeNode root) {
+            if (root.right != null) {
+                return findLeftMax(root.right);
+            }
+            return root;
+        }
+
+        public TreeNode findRightMin(TreeNode root) {
+            if (root.left != null) {
+                return findRightMin(root.left);
+            }
+            return root;
+        }
+
+        public void insertNum(int num, TreeNode root) {
+            if (num < root.val) {
+                if (root.left == null) {
+                    root.left = new TreeNode(num, root);
                 } else {
                     insertNum(num, root.left);
                 }
             } else {
-                if (null == root.right) {
-                    root.right = new TreeNode(num);
+                if (root.right == null) {
+                    root.right = new TreeNode(num, root);
                 } else {
                     insertNum(num, root.right);
                 }
             }
         }
 
+
+        public void reBalance() {
+            if (leftNum == rightNum + 1) {
+                TreeNode leftMaxNode = findLeftMax(root.left);
+                if (leftMaxNode == root.left) {
+                    leftMaxNode.right = root;
+                    root.father = leftMaxNode;
+                    leftMaxNode.father = null;
+                    root.left = null;
+                    root = leftMaxNode;
+                    leftNum--;
+                    rightNum++;
+                    return;
+                }
+                leftMaxNode.father.right = null;
+                if (leftMaxNode.left != null) {
+                    leftMaxNode.left.father = leftMaxNode.father;
+                    leftMaxNode.father.right = leftMaxNode.left;
+                }
+                leftMaxNode.left = root.left;
+
+                leftMaxNode.father = null;
+
+                leftMaxNode.right = root;
+                root.father = leftMaxNode;
+
+                root.left = null;
+
+                root = leftMaxNode;
+                leftNum--;
+                rightNum++;
+            }
+            if (rightNum == leftNum + 2) {
+                TreeNode rightMinNode = findRightMin(root.right);
+                if (rightMinNode == root.right) {
+                    rightMinNode.left = root;
+                    root.father = rightMinNode;
+                    rightMinNode.father = null;
+                    root.right = null;
+                    root = rightMinNode;
+                    rightNum--;
+                    leftNum++;
+                    return;
+                }
+                rightMinNode.father.left = null;
+                if (rightMinNode.right != null) {
+                    rightMinNode.right.father = rightMinNode.father;
+                    rightMinNode.father.left = rightMinNode.right;
+                }
+
+                rightMinNode.right = root.right;
+
+                rightMinNode.father = null;
+                rightMinNode.left = root;
+                root.father = rightMinNode;
+                root.right = null;
+                root = rightMinNode;
+                rightNum--;
+                leftNum++;
+            }
+        }
+
+
         public double findMedian() {
             if (oddFlag) {
                 return (double) root.val;
             }
-            return (double) (root.val + root.right.val) / 2;
+            return (double) (root.val + findRightMin(root.right).val) / 2;
         }
     }
 
@@ -130,8 +217,9 @@ public class Problem295 {
         MedianFinder medianFinder = new MedianFinder();
 
         Double result;
-        for (int i = -1; i > -6; i--) {
-            medianFinder.addNum(i);
+        List<Integer> numList = Arrays.asList(6, 10, 2, 6, 5, 0, 6, 3, 1, 0, 0, 6, 3, 1, 0, 0);
+        for (Integer integer : numList) {
+            medianFinder.addNum(integer);
             result = medianFinder.findMedian();
             System.out.println(result);
         }
